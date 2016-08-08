5ae4a27123a41ac711ef5a351f3d63b4c16f5122
marionline03 <marionline@openmailbox.org>
2016-08-08 16:01:07 +0200

    oberserver-pattern-test, maybe done correctly, maybe not

---
+++ my_cookbook/observer_pattern_test.py
@@ -0,0 +1,37 @@
+# Versuch das Observerpattern umzusetzen über das GUI und Controller kommunizieren sollen.
+
+class Observer:
+    def __init__(self, name):
+        self.name = name
+        pass 
+    
+    def receive(self, data):
+        print("{}: I got {}".format(self.name, data))
+
+
+class Subject():
+    def __init__(self, name):
+        self.name = name
+        self.clients = []
+
+    def send(self, data):
+        for client in self.clients:
+            if client isinstance(Observer): # isinstance
+                client.receive(data)
+            
+    def remove_client(client):
+        #TODO: wie löscht man nochmal elemente aus listen?  
+        pass
+        
+    def add_client(client):
+        if client isinstance(client, Observer):
+            self.clients.append(client)
+        else:
+            raise TypeError    
+        
+a = Observer('A')
+b = Subject('B')
+b.add_client(a)
+b.send('bla bla')
+b.add_client(Observer('C'))
+b.send("lala Laa!")
