32210934c1c8d7252fe6532d42a624bd14e5bd6d
marionline03 <marionline@openmailbox.org>
2016-08-08 18:12:26 +0200

    ein gescheiterter versuch :(

---
+++ my_cookbook/test_bottle_class_with_timer.py
@@ -0,0 +1,71 @@
+from bottle import Bottle, route, run, template, debug
+import time
+import datetime
+
+
+class Observer:
+    def __init__(self):
+        pass 
+    
+    def receive(self, data):
+        print("I got {}".format(data))
+
+
+class Subject():
+    def __init__(self):
+        self.clients = []
+
+    def send(self, data):
+        for client in self.clients:
+                client.receive(data)
+            
+    def add_client(self, client):
+            self.clients.append(client)
+        
+        
+# frei nach:
+# automate the boring stuff chapter 15
+# jede Sekunde wird die Zeit geprintet
+
+class Timer(Subject):
+    def __init__(self):
+        super().__init__()
+        self.clients = []
+        self.lasttime = round(time.time())
+
+    def run(self):
+        while True:
+            if round(time.time()) != self.lasttime:
+                self.lasttime= round(time.time())
+                self.send(self.lasttime)
+
+##read: http://stackoverflow.com/questions/8725605/bottle-framework-and-oop-using-method-instead-of-function
+class MyApp(Bottle):
+    def __init__(self, name):
+        super(MyApp, self).__init__()
+        self.name = name
+        self.time=time.time()
+        self.route('/', callback=self.index)
+        self.route('/hello', callback=self.hello)
+    
+    def index(self):
+        return "Hello, my name is " + self.name
+        
+    def receive(self, data):
+        print("data: ",data)
+        self.hello(self.time)
+        
+    def hello(self, time=time.time()): 
+        return '<h1>hello world =D!<br> {}</h1>'.format(time)  
+
+if __name__ == '__main__':
+    app = MyApp('My-App')
+    timer = Timer()
+    timer.add_client(app)
+    timer.send('test')
+    
+    # diese beiden run befehle blockieren sich gegenseitig. Juhuu! -_-
+    app.run(host='localhost', port=8080)
+    timer.run() 
+    # wird nicht angezeigt ...
+    timer.send('test 02')
