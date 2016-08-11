958e9ff2da537e2ee53b291dd229e23f4e8057ea
marionline03 <marionline@openmailbox.org>
2016-08-11 09:53:57 +0200

    working on mockup version

--- my_cookbook/mockup.py
+++ my_cookbook/mockup.py
@@ -5,6 +5,31 @@ import bottle
 from bottle import route, run, template, static_file
 from do_not_upload import STATIC_FILES_PATH
 
+@route('inventory')
+def show_inventory():
+    pass
+    
+
+# if user klicks something
+@route('/inventory')
+def handle_inventory_event():
+    # get from input
+    pass
+    # what form input? use, delete? 
+    
+    # on item use 
+        #return #gamescreen #with eating monster animation
+    # on item deletion  
+        #return #inventory
+       
+@route('/wordlist')
+def show_wordlist():
+    # request voc data
+    
+    # make displayable voc data
+    
+    return template('table goes here')
+
 @route('/<filename>')
 def serve_static(filename):
     return static_file(filename, root=STATIC_FILES_PATH)
@@ -15,6 +40,6 @@ def give_static(filename):
 
 @route('/')
 def hello():
-    return template('page', menu=[('Words','a'),('Pet','b'),('Items','c'),('Player','d')], heading='Hello world =D!', content='bla bla bla')
+    return template('page', menu=[('Learn','/learnmode'),('items','/inventory'),('words', '/wordlist'),('Buy','/buy'),('Player','/settings')], pet_image='dummy.png', content='bla bla bla')
 
 run(host='localhost', port=8080, reloader=True)
